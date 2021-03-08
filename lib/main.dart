b9729e9fb70633d2f4ff5434dbe7c9ce23c04102
Tharun <tharunedara@gmail.com>
2021-03-08 20:06:00 +0530

    Added States

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/loading.dart';
 import 'pages/home.dart';
 
 void main()=> runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/' : (context) => Loading(),
+    '/home' : (context) => Home(),
+    '/location' : (context) => ChooseLocation()
+  },
 ));
